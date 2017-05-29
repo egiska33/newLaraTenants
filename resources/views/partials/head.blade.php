3b30763863635e5ccd528e4a5244b8343d846626
Vytautas Jašinskas <j.vytautas09@gmail.com>
2017-05-29 20:15:40 +0300

    Generated home.css for home pages

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -37,3 +37,4 @@
       href="https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-timepicker-addon.min.css"/>
 <link rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.standalone.min.css"/>
+<link rel="stylesheet" href="{{ url('css') }}/home.css">
