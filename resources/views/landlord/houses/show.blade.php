def488d7547002ddb5125d71b494cebe4ab97600
Vytautas Jašinskas <j.vytautas09@gmail.com>
2017-06-07 18:54:16 +0300

    fixed css

--- resources/views/landlord/houses/show.blade.php
+++ resources/views/landlord/houses/show.blade.php
@@ -17,41 +17,29 @@
             </div>
         </div>
         @if(!$tenant->isEmpty())
-            <div class="row">
-                <p></p>
-                <div class="stats-buttons-grid">
-                    <div class="row">
-                        <div class="col-lg-4 ">
-                            <div class="thumbnail" align="center">
+            <div class="container">
+                <div class="row">
+                    <p></p>
+                    <div class="stats-buttons-grid">
+                        <div class="row">
+                            <div class="col-lg-4 ">
+                                <div class="thumbnail" align="center">
 
-                                <div class="icon green">
-                                    <i class="fa fa-file-text-o" style="font-size: 72px; margin-top: 7px;"> <img
-                                                id="profile-img" class="profile-img-card"
-                                                src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"/></i>
-                                </div>
-                                <div class="caption">
-                                    <h3>{{$tenant[0]->name}}</h3>
-                                    <p>{{$tenant[0]->email}}</p>
-                                    {{--<p align="center">--}}
-                                        {{--<button class="btn btn-danger tip delete-action" title=""><i--}}
-                                                    {{--class="fa fa-trash-o"></i>--}}
-                                        {{--@can('house_delete')--}}
-                                            {{--{!! Form::open(array(--}}
-                                                {{--'style' => 'display: inline-block;',--}}
-                                                {{--'method' => 'DELETE',--}}
-                                                {{--'onsubmit' => "return confirm('".trans("quickadmin.qa_are_you_sure")."');",--}}
-                                                {{--'route' => ['admin.users.destroy', $tenant[0]->id])) !!}--}}
-                                            {{--{!! Form::submit(trans('quickadmin.qa_delete'), array('class' => 'btn btn-xs btn-danger')) !!}--}}
-                                            {{--{!! Form::close() !!}--}}
-                                        {{--@endcan--}}
-                                    {{--</button>--}}
-                                        {{--<a href="#" class="btn btn-info tip" title=""><i--}}
-                                                    {{--class="fa fa-cloud-download"></i> View</a>--}}
-                                    {{--</p>--}}
+                                    <div class="icon green">
+                                         <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+                                    </div>
+                                    <div class="caption">
+                                        <h3>Title</h3>
+                                        <p>text</p>
+                                        <p align="center">
+                                            <a href="#" class="btn btn-danger tip delete-action" title=""><i class="fa fa-trash-o"></i> Delete</a>
+                                            <a href="#" class="btn btn-info tip" title=""><i class="fa fa-cloud-download"></i> View</a>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
