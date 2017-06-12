4169183cd126903c0148a72596d7beaf9f974bb8
egiska33 <esivickas@gmail.com>
2017-06-12 22:54:56 +0300

    from tenants page can go to tenent House

--- app/Http/Controllers/Landlord/HousesController.php
+++ app/Http/Controllers/Landlord/HousesController.php
@@ -9,13 +9,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 
-class HousesController extends Controller
-{
+class HousesController extends Controller {
+
     public function show($id)
     {
-        $users = User::where('id',$id)->get();
+        $users = User::where('id', $id)->get();
         $house = House::where('tenant_id', $id)->first();
-//        dd($users);
+
         return view('landlord.houses.show', compact('house', 'users'));
     }
 }
