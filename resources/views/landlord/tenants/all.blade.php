28ad8281857fec7abc08253a43bf977ab5b936b7
egiska33 <esivickas@gmail.com>
2017-06-13 22:08:31 +0300

    landlord.tenants.all.blade make

--- resources/views/landlord/tenants/all.blade.php
+++ resources/views/landlord/tenants/all.blade.php
@@ -2,20 +2,39 @@
 
 @section('content')
     @if(!$tenants->isEmpty())
+        <div class="container">
+            <div class="row">
+                <div class="stats-buttons-grid">
+                    <div class="row">
+                        @foreach($tenants as $tenant)
+                            <a href="{{route('show.tenant.house', $tenant->id)}}" class="userCard">
+                                <div class="col-md-4 ">
+                                    <div class="thumbnail" align="center">
 
-        @foreach($tenants as $tenant)
-            <a href="{{route('show.tenant.house', $tenant->id)}}"><b> {{$tenant->name}}:</b></a>
-            <br>
-            {{$tenant->email}}{{$tenant->phone}}
-            <hr>
+                                        <div class="icon green">
+                                            <img id="profile-img" class="profile-img-card"
+                                                 src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"/>
+                                        </div>
+                                        <div class="caption">
+                                            <h3>{{$tenant->name}}</h3>
+                                            <p>{{$tenant->email}}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
 
-        @endforeach
 
-    @else
-        <div class="col-lg-4">
-            <h3>Houses don't have Tenants</h3>
+                        @endforeach
+
+                        @else
+                            <div class="col-lg-4">
+                                <h3>Houses don't have Tenants</h3>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
         </div>
-    @endif
 
 
 @endsection
